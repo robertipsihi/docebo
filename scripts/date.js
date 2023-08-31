6120428853fd526b1c3b914b6c5e4d527e8c75e6
Robert Ipsihi <71655690+robertipsihi@users.noreply.github.com>
2023-08-31 12:38:59 +0300

    Add files via upload

---
+++ scripts/date.js
@@ -0,0 +1,32 @@
+function countdown() {
+    let now = new Date();
+    let evenDate = new Date('September 6, 2023 16:00:00 GMT+03:00');
+  
+    let actualTime = now.getTime();
+    let eventTime = evenDate.getTime();
+    let remTime = eventTime - actualTime;
+  
+    let s = Math.floor(remTime / 1000);
+    let m = Math.floor(s / 60);
+    let h = Math.floor(m / 60);
+    let d = Math.floor(h / 24);
+  
+    h %= 24;
+    m %= 60;
+    s %= 60;
+  
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+  
+    document.querySelector('#days').textContent = d;
+    document.querySelector('#hours').textContent = h;
+    document.querySelector('#minutes').textContent = m;
+    document.querySelector('#seconds').textContent = s;
+  
+    setTimeout(countdown, 1000)
+  
+  
+  };
+  
+  countdown();
